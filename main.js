0367a1e741cb9a7719d8df158e5db3d723f89726
MWest352 <MWest35285@gmail.com>
2022-04-09 21:05:13 -0600

    add board creation and fix typo in HTML

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 
 //Board Creation
+const makeBoard = (() => {
+  let blockNumber = 0
 
+  const createElement = (() => {
+    let getBoard = document.querySelector(".board")
+    let tagName = "div"
+    let createSquare = document.createElement(tagName);
+  
+    let numberBlock = createSquare.classList.add("Block" + blockNumber);
+    let appendSquare = getBoard.appendChild(createSquare)
+
+    createGrid = () =>{
+      createSquare;
+      numberBlock;
+      appendSquare;
+  }
+  return createGrid();
+  })
+
+  for (i = 0; i < 10; i++){
+    createElement();
+    blockNumber++;
+  }
+});
+makeBoard();
 
