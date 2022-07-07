3bcf2c9784e1310db41a724941a27e666832873b
MWest352 <MWest35285@gmail.com>
2022-07-07 17:45:04 -0600

    update checkWin with quick and dirty solution

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const makeBoard = (() => {
     let tagName = 'div';
     let createSquare = document.createElement(tagName);
     createSquare.classList.add('square');
-    createSquare.setAttribute('id', 'square#' + squareNumber);
+    createSquare.setAttribute('id', 'square' + squareNumber);
     getBoard.appendChild(createSquare);
   });
 
@@ -38,9 +38,7 @@ const gameLogic = (() => {
     document.addEventListener('click', function(e) {
       if ((e.target.hasAttribute('id')) == true && e.target.textContent == ''){
         e.target.textContent += currentPlayer.symbol;
-        switchPlayer();
-        switchPlayerText();
-
+        checkWin();
       }
       else {
         console.log("Invalid click for gameplay")
@@ -59,6 +57,69 @@ const gameLogic = (() => {
     } else if (currentPlayer == playerTwo) {
       currentPlayer = playerOne;
     }
+  }
+
+    //This checks the win conditions.  I hate this and will figure something else out, it was just the quick and dirty way to do it.  Suggestions are welcome.
+    function checkWin(){
+      //TOP ROW
+      if (document.querySelector('#square1').textContent.includes(currentPlayer.symbol) &&
+         (document.querySelector('#square2').textContent.includes(currentPlayer.symbol) &&
+         (document.querySelector('#square3').textContent.includes(currentPlayer.symbol)))){
+      playerTextWin();
+      }
+      //MIDDLE ROW
+      else if (document.querySelector('#square4').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square5').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square6').textContent.includes(currentPlayer.symbol)))){
+      playerTextWin();
+      }
+      //BOTTOM ROW
+      else if (document.querySelector('#square7').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square8').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square9').textContent.includes(currentPlayer.symbol)))){
+      playerTextWin();
+      }
+      //COLUMN 1
+      else if (document.querySelector('#square1').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square4').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square7').textContent.includes(currentPlayer.symbol)))){
+      playerTextWin();
+      }
+      //COLUMN 2
+      else if (document.querySelector('#square2').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square5').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square8').textContent.includes(currentPlayer.symbol)))){
+      playerTextWin();
+      }
+      //COLUMN 3
+      else if (document.querySelector('#square3').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square6').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square9').textContent.includes(currentPlayer.symbol)))){
+                                      //Damn good time
+      playerTextWin();
+      }
+      //DIAGONAL 1
+      else if (document.querySelector('#square1').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square5').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square9').textContent.includes(currentPlayer.symbol)))){
+      playerTextWin();
+      }
+      //DIAGONAL 2
+      else if (document.querySelector('#square3').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square5').textContent.includes(currentPlayer.symbol) &&
+              (document.querySelector('#square7').textContent.includes(currentPlayer.symbol)))){
+      playerTextWin();
+      }
+     else{
+      moveAlong();
+    }
+  }
+  
+  
+  //These aren't the droids you're looking for...Move Along
+  function moveAlong(){
+    switchPlayer();
+    switchPlayerText();
   }
 findElement();
   
