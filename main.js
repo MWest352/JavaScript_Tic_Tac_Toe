642a774924f741e7f3ebcd9ab2611b2e2f87a396
MWest352 <MWest35285@gmail.com>
2022-07-29 19:06:32 -0600

    Changes to reset button for clearing board and resetting player status to player one

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const makeBoard = (() => {
   let squareNumber = 1;
 
+  //SQUARE CREATOR
   const createElement = (() => {
     let getBoard = document.querySelector('.board');
     let tagName = 'div';
@@ -11,60 +12,52 @@ const makeBoard = (() => {
     getBoard.appendChild(createSquare);
   });
 
-  for (i = 1; i <= 9 ; i++){
+  for (i = 1; i <= 9 ; i++) {
     createElement();
     squareNumber++;
   };
 });
 makeBoard();
 
-
+ 
 //Player Factory
 const playerFactory = (name, symbol) => {
     return {name, symbol};
 };
 
 
-
-//Game Logic
+//MAIN LOGIC
 const gameLogic = (() => {
   let playerOne = playerFactory('Player One', 'X');
   let playerTwo = playerFactory('Player Two', 'O');
   let currentPlayer =  playerOne;
-  switchPlayerText();
+  switchPlayerText()
+  console.log(currentPlayer);
+  
 
 //Checks for element ID and text content, then places player symbol
-  function findElement() { 
+  // SQUARE FINDER
+  function findElement() {
     document.addEventListener('click', function(e) {
-      if ((e.target.hasAttribute('id')) == true && e.target.textContent == ''){
-        e.target.textContent += currentPlayer.symbol;
-        checkWin();
+      if (e.target.id == "resetGame"){
+          clearBoard();
+          currentPlayer = playerOne;
+        }
+        else if ((e.target.hasAttribute('id')) == true && e.target.textContent == ''){
+        console.log(e.target)
+        e.target.textContent += currentPlayer.symbol; 
+        checkWin();  
       }
       else {
+        console.log(e.target)
         console.log("Invalid click for gameplay")
-      }
+        
+      };
     });
   };
 
-  function switchPlayerText() {
-    document.getElementById("playerUp").textContent = `${currentPlayer.name}, please choose a square.`
-  };
-
-  function playerTextWin() {
-    document.getElementById("playerUp").textContent = `${currentPlayer.name} wins!`
-  };
-  
-  //Switches from Player 1 to Player 2
-  function switchPlayer() {
-    if (currentPlayer == playerOne) {
-      currentPlayer = playerTwo;
-    } else if (currentPlayer == playerTwo) {
-      currentPlayer = playerOne;
-    }
-  }
-
-    //This checks the win conditions.  I hate this and will figure something else out, it was just the quick and dirty way to do it.  Suggestions are welcome.
-    function checkWin(){
+  //This checks the win conditions.  I hate this and will figure something else out, it was just the quick and dirty way to do it.  Suggestions are welcome.
+  function checkWin(){
       //TOP ROW
       if (document.querySelector('#square1').textContent.includes(currentPlayer.symbol) &&
          (document.querySelector('#square2').textContent.includes(currentPlayer.symbol) &&
@@ -119,16 +112,42 @@ const gameLogic = (() => {
     }
   }
   
-  
-  //These aren't the droids you're looking for...Move Along
   function moveAlong(){
+    console.log("Move Along")
+    console.log(currentPlayer);
     switchPlayer();
+    console.log(currentPlayer);
     switchPlayerText();
-  }
+  };
+  
+  //Updates Current Player Display
+  function switchPlayerText() {
+    document.getElementById("playerUp").textContent = `${currentPlayer.name}, please choose a square.`
+  };
+
+  //Updates Current Player Display For Win
+  function playerTextWin() {
+    document.getElementById("playerUp").textContent = `${currentPlayer.name}, wins!`
+  };
+  
+  //Switches from Player 1 to Player 2
+  function switchPlayer() {
+    if (currentPlayer == playerOne) {
+      currentPlayer = playerTwo;
+    } else if (currentPlayer == playerTwo) {
+      currentPlayer = playerOne;
+    }
+  };
 findElement();
-  
-  
-  
 });
 gameLogic();
 
+//Reset Game
+function clearBoard() {
+  console.log("Clear Board")
+  var square = document.querySelectorAll(".square");
+
+  for (var i = 0; i < square.length; i++) {
+    square[i].textContent = '';
+  }
+};
